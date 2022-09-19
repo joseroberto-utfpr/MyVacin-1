fb96436975e4af03acd20d0d9f4be6c51a1f7587
José Roberto <joseroberto@alunos.utfpr.edu.br>
2022-09-19 17:52:58 +0000

    Create 13 files

---
+++ build/precache-manifest.9f6a99eca819f0ffbac76b5e2f69b1d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b8a70d93e1a4e462877fab75784c53c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74228b86959960f3ed18",
+    "url": "/static/js/2.5d72959a.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.5d72959a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7ef169be1a83d373ac5",
+    "url": "/static/js/main.44c47536.chunk.js"
+  },
+  {
+    "revision": "1cba36deef3a7d2b858a",
+    "url": "/static/js/runtime-main.00c5e145.js"
+  }
+]);
